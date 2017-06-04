e6698b7a13280f6388c51adf16eb9b908386011f
Oriol Abril: antarctica <oriol.abril.pla@gmail.com>
2017-06-04 22:31:18 +0200

    vim-inlist.py updated to write the whole inlivt.vim file

--- vim-inlist.py
+++ vim-inlist.py
@@ -1,16 +1,18 @@
-import re
+import re,os
 
 keypat=re.compile(r'\s*([^\s!:()]+)\s*=',re.IGNORECASE)
 boolpat=re.compile(r'\s*([^\s!:()]+)\s*=\s*.(true|false).',re.IGNORECASE)
 intpat=re.compile(r'\s*([^\s!:()]+)\s*=\s*[0-9\-d\.]+',re.IGNORECASE)
 matpat=re.compile(r'\s*([^\s!]+)\([:0-9]\)\s*=',re.IGNORECASE)
-fd=open('controls.keywords','w')
+fd=open('inlist.vim','w')
+folder=os.environ['MESA_DIR']+'/star/defaults/'
 keys=set()
 mats=set()
 bols=set()
 ints=set()
+
 ##controls.defaults
-f1=open('controls.defaults','r')
+f1=open(folder+'controls.defaults','r')
 for line in f1:
     b=boolpat.match(line)
     i=intpat.match(line)
@@ -28,7 +30,7 @@ for line in f1:
 f1.close()
 
 ##star_job.defaults
-f2=open('star_job.defaults','r')
+f2=open(folder+'star_job.defaults','r')
 for line in f2:
     b=boolpat.match(line)
     i=intpat.match(line)
@@ -46,7 +48,7 @@ for line in f2:
 f2.close()
 
 ##pgstar.defaults
-f3=open('pgstar.defaults','r')
+f3=open(folder+'pgstar.defaults','r')
 for line in f3:
     b=boolpat.match(line)
     i=intpat.match(line)
@@ -63,6 +65,10 @@ for line in f3:
         mats.add(n.groups(1)[0])
 f3.close()
 
+#write inlist.vim
+fd.write('if exists("b:current_syntax")\n   finish\nendif\n\n"define keywords (obtained from controls.defaults, star_job.defaults and pgstar.defaults)\
+"some of the keywords will only highlight if they are followed by the corresponding boolean or number\nsyntax case ignore\n')
+
 #write obtained keywords in file
 for key in keys:
     string='syntax keyword inlistKeyword '+key+'\n'
@@ -78,4 +84,23 @@ for integ in ints:
 for mat in mats:
     string=r'syntax match inlistKeyword /\zs'+mat+'\ze([0-9]*)/\n'
     fd.write(string)
+
+fd.write('highlight link inlistKeyword Keyword\n\n"define comments\n')
+fd.write(r'syntax match inlistComment "\v!.*$"')
+fd.write('\nhighlight link inlistComment Comment\n\n"define the headers of the different sections in an inlist as Identifiers\n')
+fd.write('syntax match inlistHeader "\\v\&controls"\nsyntax match inlistHeader "\\v\&star_job"\n')
+fd.write('syntax match inlistHeader "\\v^\/"\nsyntax match inlistHeader "\\v\&pgstar"\nhighlight link inlistHeader Identifier\n')
+fd.write('\n"Numbers of various sorts\n"" Integers\nsyn match inlistNumber display "\<\d\+\(_\\a\w*\)\=\>"\n')
+fd.write('" floating point number, without a decimal point\nsyn match inlistNumber display "\<\d\+[deq][-+]\=\d\+\(_\\a\w*\)\=\>"')
+fd.write('\n" " floating point number, starting with a decimal point\nsyn match inlistNumber display "\.\d\+\([deq][-+]\=\d\+\)\=\(_\\a\w*\)\=\>"\n')
+fd.write('" " floating point number, no digits after decimal\nsyn match inlistNumber display "\<\d\+\.\([deq][-+]\=\d\+\)\=\(_\\a\w*\)\=\>"\n')
+fd.write('" " floating point number, D or Q exponents\nsyn match inlistNumber display "\<\d\+\.\d\+\([dq][-+]\=\d\+\)\=\(_\a\w*\)\=\>"\n')
+fd.write('" " floating point number\nsyn match inlistNumber  display "\<\d\+\.\d\+\(e[-+]\=\d\+\)\=\(_\\a\w*\)\=\>"\n')
+fd.write('highlight link inlistNumber Number\n\n')
+fd.write('"define booleans with the dots\nsyntax match inlistBoolean ".true."\n')
+fd.write('syntax match inlistBoolean ".false."\nhighlight link inlistBoolean Boolean\n')
+fd.write('"define strings\n')
+fd.write(r'''syntax region inlistString start=/\v'/ skip=/\v\\./ end=/\v'/''')
+fd.write('\nhighlight link inlistString String\n\n')
+fd.write('let b:current_syntax = "inlist"')
 fd.close()
